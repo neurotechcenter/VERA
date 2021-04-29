2032ecc8f0f02274c9c63b2d15bc269f25c6e808
James Swift <swift@neurotechcenter.org>
2021-04-29 13:14:40 -0500

    Updated startup file name, added standard subject processing pipeline

---
+++ startup_VERA.m
@@ -0,0 +1,12 @@
+close all;
+clearvars;
+
+restoredefaultpath;
+
+addpath(genpath('classes'));
+addpath(genpath('Components'));
+addpath(genpath('Dependencies'));
+javaaddpath('Dependencies/Widgets Toolbox/resource/MathWorksConsultingWidgets.jar');
+import uiextras.jTree.*;
+guihandle=MainGUI();
+addToolbarExplorationButtons(guihandle);
