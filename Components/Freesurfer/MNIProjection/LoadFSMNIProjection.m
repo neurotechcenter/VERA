c11cb17049404fd7290a30240740ff2d3a18c8e9
Markus Adamek <adamek@neurotechcenter.org>
2022-04-25 16:12:05 -0500

    New component to project FS Data into MNI space

---
+++ Components/Freesurfer/MNIProjection/LoadFSMNIProjection.m
@@ -0,0 +1,44 @@
+classdef LoadFSMNIProjection < AComponent
+    %FREESURFERMNIPROJECTION
+    % Load transformation matrix for FS MNI305
+    
+    properties
+       SegmentationPathIdentifier
+       TIdentifier
+
+    end
+    
+    methods
+        function obj = LoadFSMNIProjection()
+            obj.SegmentationPathIdentifier='SegmentationPath';
+            obj.TIdentifier='T_MNI';
+        end
+        
+        function Publish(obj)
+            obj.AddOptionalInput(obj.SegmentationPathIdentifier,'PathInformation',true);
+            obj.AddOutput(obj.TIdentifier,'TransformationMatrix');
+        end
+        
+        function Initialize(obj)
+            path=obj.GetDependency('Freesurfer');
+            addpath(fullfile(path,'matlab'));
+        end
+        
+        function T=Process(obj,optInp)
+            if(nargin > 1) %segmentation path exists
+                segmentationPath=optInp.Path;
+                comPath=fileparts(obj.ComponentPath);
+                segmentationPath=fullfile(comPath,segmentationPath); %create full path
+            else
+                segmentationPath=uigetdir([],'Please select Freesurfer Segmentation');
+                if(isempty(segmentationPath))
+                    error('No path selected!');
+                end
+            end
+            T=obj.CreateOutput(obj.TIdentifier);
+            T.T=xfm_read(fullfile(segmentationPath,'mri','transforms','talairach.xfm'));
+            
+        end
+    end
+end
+
