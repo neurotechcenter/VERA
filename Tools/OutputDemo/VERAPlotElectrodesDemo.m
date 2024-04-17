fb674610159a1d95d32577c96632e78efddddf55
James Swift <jamesswift@Jamess-MacBook-Pro.local>
2024-04-17 15:57:05 -0600

    cleaned up output demo

--- Tools/OutputDemo/VERAPlotElectrodesDemo.m
+++ Tools/OutputDemo/VERAPlotElectrodesDemo.m
@@ -15,26 +15,65 @@ end
 
 brainmat = load(fullfile(filepath,filename));
 
-elec_radius = 0.5;
+%% Create surface annotation colors
+[surfRemap,surfcmap,surfNames,surfName_id] = createColormapFromAnnotations(brainmat.surfaceModel);
+
+%% Create electrode annotation colors
+electrodeLabels       = cellfun(@(x)x{1},brainmat.electrodes.Label,'UniformOutput',false); % convert to easier to use cell array
+uniqueElectrodeLabels = unique(electrodeLabels);
+electrodeColors       = distinguishable_colors(length(uniqueElectrodeLabels));
+electrodeRadius       = 0.75;
+
+%% Plot implanted electrodes on brain model, using electrode labels
+% Plot brain model using VERA function (uses trisurf)
+
+fullcolorFig = figure;
+plot3DModel(gca,brainmat.surfaceModel.Model,surfRemap);
+colormap(surfcmap); % colorize with annotation for Surface
+alpha(0.05);
+hold on;
+% Plot electrode numbering
+plotElNums(brainmat.electrodes.Location*1.075, 1:size(brainmat.electrodes.Location,1), 12);
+% Plot electrodes colorized by brain area (annotation)
+for i = 1:length(uniqueElectrodeLabels)
+    % Find the electrodes that belong to each unique label and color them appropriately
+    elecsToPlot = strcmp(uniqueElectrodeLabels{i},electrodeLabels);
+    plotBallsOnVolume(gca,brainmat.electrodes.Location(elecsToPlot,:),electrodeColors(i,:),electrodeRadius);
+end
+view(-114,25)
+
+% Modify colorbar to include more detailed labeling (surface labels on bottom, electrode labels on top)
+cb                      = colorbar;
+cb.Ticks                = [surfName_id; (surfName_id(end)+1:1:surfName_id(end)+length(uniqueElectrodeLabels))']+0.5;
+cb.TickLabels           = [surfNames(:); uniqueElectrodeLabels(:)];
+cb.TickLabelInterpreter = 'none';
+clim([surfName_id(1) surfName_id(end)+length(uniqueElectrodeLabels)+1])
+
+% Save full color figure
+saveas(fullcolorFig,[filepath,filename(1:end-4),'.fig'])
+
 
 %% Plot implanted electrodes on brain model
-% Create color map from annotations (surface labels)
-[remap,cmap,names,name_id] = createColormapFromAnnotations(brainmat.surfaceModel);
-
 % Plot brain model using VERA function (uses trisurf)
+
 figure;
-plot3DModel(gca,brainmat.surfaceModel.Model,remap);
-colormap(cmap); % colorize with annotation for Surface
-cb = colorbar('Ticks',[name_id; name_id(end)+1]+0.5,'TickLabels',[names; {'electrodes'}]);
-clim([1 name_id(end)+2]) % add 2, one for unknown, 1 for electrodes
-cb.TickLabelInterpreter = 'none';
+plot3DModel(gca,brainmat.surfaceModel.Model,surfRemap);
+colormap(surfcmap); % colorize with annotation for Surface
 alpha(0.05);
 hold on;
 % Plot electrodes in one color
-plotBallsOnVolume(gca,brainmat.electrodes.Location,[],elec_radius);
+plotBallsOnVolume(gca,brainmat.electrodes.Location,[],electrodeRadius);
+view(-114,25)
 
+% Colorbar with only surface labels
+cb                      = colorbar;
+cb.Ticks                = [surfName_id; surfName_id(end)+1]+0.5;
+cb.TickLabels           = [surfNames; {'electrodes'}];
+cb.TickLabelInterpreter = 'none';
+clim([1 surfName_id(end)+2]) % add 2, one for unknown, 1 for 'electrodes'
 
-%% Plot brain model with sticks
+%% Plot brain model with trajectories
+% Plot grayscale brain model with sticks identifying implant trajectories
 NumImplants = size(brainmat.electrodes.Definition,1);
 NumElecs    = [brainmat.electrodes.Definition.NElectrodes];
 implantcmap = jet(NumImplants);
@@ -43,7 +82,7 @@ figure;
 plot3DModel(gca,brainmat.surfaceModel.Model,[]);
 alpha(0.3);
 hold on;
-elecCtr  = 1;
+elecCtr = 1;
 for i = 1:NumImplants
     plot3([brainmat.electrodes.Location(elecCtr,1), brainmat.electrodes.Location(elecCtr + NumElecs(i)-1,1)],...
           [brainmat.electrodes.Location(elecCtr,2), brainmat.electrodes.Location(elecCtr + NumElecs(i)-1,2)],...
@@ -52,70 +91,39 @@ for i = 1:NumImplants
 
     elecCtr = elecCtr + NumElecs(i);
 end
-colormap([0.5 0.5 0.5;implantcmap]); 
-cb = colorbar('Ticks',[0:NumImplants]+1.5,'TickLabels',{'cortex',brainmat.electrodes.Definition.Name});
-clim([1 NumImplants+2])
-cb.TickLabelInterpreter = 'none';
-
-%% Plot implanted electrodes on brain model, using electrode labels
-% Use secondary labels (more specific) for electrode locations (typically volume labels)
-% Secondary labels come from using the "ReplaceLabels" component
-cell_array_labels = cellfun(@(x)x{1},brainmat.electrodes.Label,'UniformOutput',false); % convert to easier to use cell array
-unique_labels     = unique(cell_array_labels);
-label_cols        = distinguishable_colors(length(unique_labels));
-
-% Plot brain model using VERA function (uses trisurf)
-fullcolor = figure;
-plot3DModel(gca,brainmat.surfaceModel.Model,remap);
-colormap(cmap); % colorize with annotation for Surface
-alpha(0.05);
-hold on;
-% Plot electrodes colorized by brain area (annotation, secondary labels)
-for i=1:length(unique_labels)
-    % Find the electrodes that belong to each unique label and color them appropriately
-    elecsToPlot = strcmp(unique_labels{i},cell_array_labels);
-    plotBallsOnVolume(gca,brainmat.electrodes.Location(elecsToPlot,:),label_cols(i,:),elec_radius);
-end
+colormap([0.5 0.5 0.5; implantcmap]); 
 view(-114,25)
 
-% Modify colorbar to include more detailed labeling (original surface labels on bottom, electrode labels on top)
-cb            = colorbar;
-cb.Ticks      = [name_id; (name_id(end)+1:1:name_id(end)+length(unique_labels))']+0.5;
-cb.TickLabels = {names{:}, unique_labels{:}}';
+% Colorbar with grayscale brain model and implant trajectories in jet
+cb                      = colorbar;
+cb.Ticks                = [0:NumImplants]+1.5;
+cb.TickLabels           = {'cortex',brainmat.electrodes.Definition.Name};
 cb.TickLabelInterpreter = 'none';
-clim([name_id(1) name_id(end) + length(unique_labels) + 1])
+clim([1 NumImplants+2])
 
 %% Plot implanted electrodes on gray brain model, using electrode labels
-% Use secondary labels (more specific) for electrode locations (typically volume labels)
-% Secondary labels come from using the "ReplaceLabels" component
-cell_array_labels = cellfun(@(x)x{1},brainmat.electrodes.Label,'UniformOutput',false); % convert to easier to use cell array
-unique_labels     = unique(cell_array_labels);
-label_cols        = distinguishable_colors(length(unique_labels));
-
 % Plot brain model using VERA function (uses trisurf)
+
 figure;
 plot3DModel(gca,brainmat.surfaceModel.Model);
 colormap([0.5 0.5 0.5]); % colorize grayscale
 alpha(0.1);
 hold on;
 plotElNums(brainmat.electrodes.Location*1.075, 1:size(brainmat.electrodes.Location,1), 12);
-% Plot electrodes colorized by brain area (secondary labels)
-for i=1:length(unique_labels)
+% Plot electrodes colorized by brain area
+for i=1:length(uniqueElectrodeLabels)
     % Find the electrodes that belong to each unique label and color them appropriately
-    elecsToPlot = strcmp(unique_labels{i},cell_array_labels);
-    plotBallsOnVolume(gca,brainmat.electrodes.Location(elecsToPlot,:),label_cols(i,:),elec_radius);
+    elecsToPlot = strcmp(uniqueElectrodeLabels{i},electrodeLabels);
+    plotBallsOnVolume(gca,brainmat.electrodes.Location(elecsToPlot,:),electrodeColors(i,:),electrodeRadius);
 end
 view(-114,25)
 
 % Modify colorbar to include electrode labeling
-cb            = colorbar;
-cb.Ticks      = [1:length(unique_labels)+1]+0.5;
-cb.TickLabels = {'cortex',unique_labels{:}}';
+cb                      = colorbar;
+cb.Ticks                = [0:length(uniqueElectrodeLabels)]+1.5;
+cb.TickLabels           = [{'cortex'}; uniqueElectrodeLabels(:)];
 cb.TickLabelInterpreter = 'none';
-clim([1 length(unique_labels) + 2])
-
-% Save the best one
-saveas(fullcolor,[filepath,filename(1:end-4),'.fig'])
+clim([1 length(uniqueElectrodeLabels) + 2])
 
 
 
