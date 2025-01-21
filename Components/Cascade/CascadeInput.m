197b48b9efcfd776c7665fa85d8f7267070eb568
James Swift <jamesswift@Jamess-MacBook-Pro-2.local>
2025-01-21 16:08:11 -0700

    fixed CascadeInput component

--- Components/Cascade/CascadeInput.m
+++ Components/Cascade/CascadeInput.m
@@ -11,38 +11,39 @@ classdef CascadeInput < AComponent
     
     methods
         function obj = CascadeInput()
-            obj.OutputIdentifiers={'Surface','ElectrodeDefinition','ElectrodeLocation'};
-            obj.OutputIdentifierTypes={'Surface','ElectrodeDefinition','ElectrodeLocation'};
+            obj.OutputIdentifiers     = {'Surface','ElectrodeDefinition','ElectrodeLocation'};
+            obj.OutputIdentifierTypes = {'Surface','ElectrodeDefinition','ElectrodeLocation'};
         end
         
         function Publish(obj)
             if(length(obj.OutputIdentifiers) ~= length(obj.OutputIdentifierTypes))
                 error('Identifiers and IdentifierTypes must be of the same length')
             end
-            for iO=1:length(obj.InputIdentifiers)
-                obj.AddOutput(obj.OutputIdentifiers{iO},obj.OutputIdentifierTypes{iO});
+            for iO = 1:length(obj.OutputIdentifiers)
+                obj.AddOutput(obj.OutputIdentifiers{iO}, obj.OutputIdentifierTypes{iO});
             end
         end
         
         function Initialize(obj)
         end
         
-        function varargout=Process(obj)
-            path=uigetdir([],'Please select VERA Project');
-            prj=Project.OpenProjectFromPath(path);
-            runner=Runner.CreateFromProject(prj);
-            varargout=cell(1,length(obj.InputIdentifier));
-            for iO=1:length(obj.InputIdentifier)
-                if(runner.CurrentPipelineData.isKey(obj.InputIdentifier{iO}) && ...
-                        isObjectTypeOf(runner.CurrentPipelineData(obj.InputIdentifier{iO}),obj.OutputIdentifierTypes{iO}))
-                     varargout{iO}=runner.CurrentPipelineData(obj.InputIdentifier{iO});
+        function varargout = Process(obj)
+            path      = uigetdir([], 'Please select VERA Project');
+            prj       = Project.OpenProjectFromPath(path);
+            runner    = Runner.CreateFromProject(prj);
+            varargout = cell(1, length(obj.OutputIdentifiers));
+
+            for iO = 1:length(obj.OutputIdentifiers)
+                if(runner.CurrentPipelineData.isKey(obj.OutputIdentifiers{iO}) && ...
+                        isObjectTypeOf(runner.CurrentPipelineData(obj.OutputIdentifiers{iO}), obj.OutputIdentifierTypes{iO}))
+
+                     varargout{iO} = runner.CurrentPipelineData(obj.OutputIdentifiers{iO});
+
                 else
-                    error(['Selected project does not have the Required Input' obj.InputIdentifier{iO}]);
+                    error(['Selected project does not have the Required Input' obj.OutputIdentifiers{iO}]);
                 end
             end
-            
         end
-
     end
 end
 
