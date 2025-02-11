7957071ccba20f577215c5407dd78c969e7d008d
James Swift <jamesswift@Jamess-MBP-2.lan>
2025-02-11 14:57:30 -0800

    transformation matrix loader

---
+++ Components/XfrmLoader.m
@@ -0,0 +1,64 @@
+classdef XfrmLoader < AComponent
+    %XfrmLoader - Component loads data from a file with a file selector
+    % Allows loading a transformation matrix as a text file
+
+    properties
+        TIdentifier char       % Data identifier
+        FileTypeWildcard char % Wildcard definition for File Loader
+        InputFilepath char
+    end
+
+    methods
+        function obj = XfrmLoader()
+            %FileLoader - Constructor
+            obj.TIdentifier      = 'T';
+            obj.FileTypeWildcard = '*.*';
+            obj.InputFilepath    = '';
+        end
+        function Publish(obj)
+            % Publish - Define Output for Component
+            % See also AComponent.Publish
+            obj.AddOutput(obj.TIdentifier,'TransformationMatrix');
+
+        end
+        function Initialize(obj)
+            % Initialize
+            % See also AComponent.Initialize
+
+        end
+
+        function [out] = Process(obj)
+            % Process - opens file selector GUI and passes the file or
+            % folder to the Data object
+            % See also AComponent.Process, IFileLoader
+            if ~isempty(obj.InputFilepath)
+
+                % working directory is VERA project
+                if isAbsolutePath(obj.InputFilepath)
+                    [path,file,ext] = fileparts(obj.InputFilepath);
+                else
+                    [path,file,ext] = fileparts(fullfile(obj.ComponentPath,'..',obj.InputFilepath));
+                    path = GetFullPath(path);
+                end
+
+                file = [file,ext];
+
+                % Open a file load dialog if you can't find the path
+                if ~exist(fullfile(path,file),'file')
+                    [file,path] = uigetfile(obj.FileTypeWildcard,['Please select ' obj.TIdentifier]);
+                end
+            else
+                [file,path] = uigetfile(obj.FileTypeWildcard,['Please select ' obj.TIdentifier]);
+            end
+
+            if isequal(file,0)
+                error([obj.TIdentifier ' selection aborted']);
+            else
+                out   = obj.CreateOutput(obj.TIdentifier);
+                out.T = load(fullfile(path,file));
+            end
+
+        end
+    end
+end
+
