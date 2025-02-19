40ee1a5de81b324610046422c069686daf58e28c
James Swift <jamesswift@Jamess-MacBook-Pro-2.local>
2025-02-19 13:22:59 -0700

    New component to take the ElectrodeDefinition as an input and create a new definition where all the NElectrodes is two. This is helpful for the new method to predict electrode locations based on manual identification of the distal contact

---
+++ Components/ElectrodeDefinition/TwoPointElectrodeDefinition.m
@@ -0,0 +1,48 @@
+classdef TwoPointElectrodeDefinition  < AComponent
+    %ElectrodeDefinitionConfiguration Component to define Electrodes (Grids, Strips, Depth Electrodes)
+    %This Component is used to define electrode configurations
+
+    properties
+        Identifier char % Identifier of Output Data, Default is 'ElectrodeDefinition'
+        TwoPointIdentifier
+        ElectrodeDefinition % Electrode Definitions
+    end
+
+    methods
+        function obj = TwoPointElectrodeDefinition()
+            %ElectrodeDefinitionConfiguration - Constructor
+            obj.Identifier          = 'ElectrodeDefinition';
+            obj.TwoPointIdentifier  = 'ElectrodeDefinition_2Points';
+            obj.ElectrodeDefinition = [];
+        end
+
+        function Publish(obj)
+            % Publish - Adds an Output for the ElectrodeDefiniton Data
+            % See also AComponent.Publish, ElectrodeDefinition
+            obj.AddInput(obj.Identifier,'ElectrodeDefinition');
+            obj.AddOutput(obj.TwoPointIdentifier,'ElectrodeDefinition');
+        end
+
+        function Initialize(obj)
+        end
+
+        function [out] = Process(obj,edef)
+            %Process - Returns a new ElectrodeConfiguration object
+            %Creates a ElectrodeConfiguration object with the information
+            %contained in the property ElectrodeConfiguration. This
+            %property is usually set in the GUI via the ElectrodeDefinition
+            %View
+            %See also AComponent.Process, ElectrodeConfiguration,
+            %ElectrodeDefinitionView
+
+            out = obj.CreateOutput(obj.TwoPointIdentifier);
+
+            out.Definition = edef.Definition;
+            for i = 1:size(out.Definition,1)
+                out.Definition(i).NElectrodes = 2;
+            end
+
+        end
+    end
+end
+
