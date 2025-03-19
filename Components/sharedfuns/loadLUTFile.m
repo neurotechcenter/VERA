6a259c1c224170bbcfbce9223f6db9004e883e42
James Swift <jamesswift@Jamess-MacBook-Pro-2.local>
2025-03-19 16:59:44 -0600

    modified loadLUTFile to output colormap from LUT

--- Components/sharedfuns/loadLUTFile.m
+++ Components/sharedfuns/loadLUTFile.m
@@ -1,15 +1,16 @@
-function [code,lut]=loadLUTFile(lutfile)
+function [code,lut,rgbv] = loadLUTFile(lutfile)
 %loadLUTFile - load Freesurfer LUT file 
 
     try
-        [code,lut]=read_fscolorlut(lutfile);
+        [code,lut,rgbv] = read_fscolorlut(lutfile);
     catch %not a fs color lut
-        [X,a,b]=importdata(lutfile); %we assume 1 is the ID, and 3 is the name
-        code=[];
-        lut={};
-        for i=2:length(X.textdata)
-            code(end+1)=str2num(X.textdata{i,1});
-            lut{end+1}=X.textdata{i,3};
+        [X,a,b] = importdata(lutfile); %we assume 1 is the ID, and 3 is the name
+        code    = [];
+        lut     = {};
+        rgbv    = [];
+        for i = 2:length(X.textdata)
+            code(end+1) = str2num(X.textdata{i,1});
+            lut{end+1}  = X.textdata{i,3};
         end
     end
 
