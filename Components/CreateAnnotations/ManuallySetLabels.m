bc113381990cc6b7e21db1a3a6c61672199fcbc0
James Swift <jamesswift@Jamess-MacBook-Pro-2.local>
2026-08-01 19:25:31 +0800

    made electrode definition a mandatory input

--- Components/CreateAnnotations/ManuallySetLabels.m
+++ Components/CreateAnnotations/ManuallySetLabels.m
@@ -16,26 +16,17 @@ classdef ManuallySetLabels < AComponent
         end
 
         function Publish(obj)
-            obj.AddInput(obj.ElectrodeLocationIdentifier,  'ElectrodeLocation');
-            obj.AddOptionalInput(obj.ElectrodeDefinitionIdentifier, 'ElectrodeDefinition');
-            obj.AddOutput(obj.ElectrodeLocationIdentifier, 'ElectrodeLocation');
+            obj.AddInput(obj.ElectrodeLocationIdentifier,   'ElectrodeLocation');
+            obj.AddInput(obj.ElectrodeDefinitionIdentifier, 'ElectrodeDefinition');
+            obj.AddOutput(obj.ElectrodeLocationIdentifier,  'ElectrodeLocation');
         end
         function Initialize(obj)
         end
 
-        function out=Process(obj,elLocs,varargin)
+        function out=Process(obj,elLocs,eDef)
             out = obj.CreateOutput(obj.ElectrodeLocationIdentifier, elLocs);
-            obj.Labels = elLocs;
-
-            if length(varargin) == 2
-                obj.ElectrodeDefinition = varargin{2};
-            else
-                obj.ElectrodeDefinition = [];
-                warning(['ManuallySetLabels: No ElectrodeDefinition input was received - Channel Name ' ...
-                    'column will be blank. Make sure the component producing ElectrodeDefinition is ' ...
-                    'positioned BEFORE ManuallySetLabels in the pipeline (this is required at pipeline ' ...
-                    'load time, independent of the order components are actually run in).']);
-            end
+            obj.Labels             = elLocs;
+            obj.ElectrodeDefinition = eDef;
 
             % create view to modify labels
             h      = figure('Name',obj.Name,'Position',[200,150,700,500]);
